17b3fe35b52466f715345afff510361010c247bf
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-24 18:08:31 +0530

    Refactor cartSlice and add checkout functionality

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -1,50 +1,82 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { getDiscountedPricePercentage } from "../utils/helper";
+import { useSelector, useDispatch } from "react-redux";
+
+import { FaCartPlus } from "react-icons/fa";
+import { addToCart, checkoutCart } from "@/store/slices/cartSlice";
+import { useNavigate } from "react-router-dom";
+
 const ProductCard = ({ data: { attributes: p, id } }) => {
+  const dispatch = useDispatch();
+
+  const [loading , setLoading] = useState(false); 
+
+  const handlebutton = () => {
+    setLoading(true);
 
 
+    console.log("Button Clicked");
+    dispatch(checkoutCart());
+    
 
+  };
 
-  
+  return (
 
-    return (
-        <Link
-            href={`/product/${p.slug}`}
-            className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
-        >
-            <Image
-                width={500}
-                height={500}
-                src={p.thumbnail.data.attributes.url}
-                alt={p.name}
-            />
-            <div className="p-4 text-black/[0.9]">
-                <h2 className="text-lg font-medium">{p.name}</h2>
-                <div className="flex items-center text-black/[0.5]">
-                    <p className="mr-2 text-lg font-semibold">
-                        ${p.price}
-                    </p>
+    <Link
+      href={`/product/${p.slug}`}
+      className="transform overflow-hidden bg-white duration-200 hover:scale-90 cursor-pointer border-gray border-1 rounded-md shadow-md flex flex-col justify-between"
+    >
 
-                    {p.original_price && (
-                        <>
-                            <p className="text-base  font-medium line-through">
-                                ${p.original_price}
-                            </p>
-                            <p className="ml-auto text-base font-medium text-green-500">
-                                {getDiscountedPricePercentage(
-                                    p.original_price,
-                                    p.price
-                                )}
-                                % off
-                            </p>
-                        </>
-                    )}
-                </div>
+{loading && (
+            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
             </div>
-        </Link>
-    );
+        )}
+      <h2 className="font-medium ml-2 text-gray-900 text-2xl ">
+        {p.name.split(" ").slice(0, 4).join(" ")}{" "}
+      </h2>
+      <button>
+        <FaCartPlus className="p-2 text-4xl text-white bg-black rounded-full ml-auto" />
+      </button>
+      <Image
+        width={500}
+        height={500}
+        src={p.thumbnail.data.attributes.url}
+        alt={p.name}
+      />
+
+      <div className="flex items-center justify-between p-4 bg-white h-[30%] margin-top-auto">
+        <div className="p-4 text-black/[0.9]">
+          <div className="flex items-center text-black/[0.5]">
+            <p className="mr-2 text-lg font-semibold">${p.price}</p>
+
+            {p.original_price && (
+              <>
+                <p className="text-base  font-medium line-through">
+                  ${p.original_price}
+                </p>
+                <p className="ml-auto text-base font-medium text-green-500">
+                  {getDiscountedPricePercentage(p.original_price, p.price)}% off
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center w-40 h-10 bg-gray-700 rounded-md object-contain hover:scale-105 transition-opacity">
+          <button
+            onClick={handlebutton}
+            className="hover:text-3xl transition-all duration-200"
+          >
+            <h2 className="text-yellow-500 text-2xl font-bold ">Buy Now</h2>
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
